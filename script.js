1383dbe4c53a5798ff3671c5be696f4738f055b1
Sami <sowusuaboagye@gmail.com>
2019-10-03 15:36:58 +0100

    change

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var str = "3 2 1 G0!!! Glossophobia or speech anxiety is the fear of public speaking. The word glossophobia derives from the Greek γλῶσσα glōssa, meaning tongue, and phobos, fear or dread. Some people have this specific phobia, while others may also have broader social phobia or social anxiety disorder.";
+var str = "3 2 1 G0!!! Nacho Libre is a comedy- drama film directed by Jared Hess inspired by the true story of a priest called Sergio Benitez who resorted to wrestling to support an orphanage in Mexico";
 var words = str.split(" ");
 
 var iterator = words.values();
