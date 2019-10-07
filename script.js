be5e557d2a46a4291755d0e869762287a57fb65a
Sami <sowusuaboagye@gmail.com>
2019-10-07 09:17:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var str = "3 2 1 G0!!! Nacho Libre is a comedy- drama film directed by Jared Hess inspired by the true story of a priest called Sergio Benitez who resorted to wrestling to support an orphanage in Mexico";
+var str = "3 2 1 G0!!!. Coma is a deep state of prolonged unconsciousness in which a person fails to respond to painful stimuli, light or sound. Patients are unable to consciously speak, feel or move. A Coma can occur by natural causes or medically induced. Done!!!";
 var words = str.split(" ");
 
 var iterator = words.values();
